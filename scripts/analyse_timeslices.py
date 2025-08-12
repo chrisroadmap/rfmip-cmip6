ea53818ca4e07af64d3c9c2887f81bdab8cd0138
Chris Smith <chrisroadmap@gmail.com>
2025-08-12 18:02:08 +0200

    update analysis script

--- scripts/analyse_timeslices.py
+++ scripts/analyse_timeslices.py
@@ -1,5 +1,6 @@
 """Script to generate ERFs from time slice experiments."""
 
+import copy
 import os
 import pickle
 
@@ -13,18 +14,17 @@ with open('../output/timeslice.pkl', 'rb') as handle:
 # don't forget to exclude first few years
 # regardless of experiment, just take the last 15
 
+experiments = ["4xCO2", "ghg", "aer", "lu", "anthro"]
+
 rfmip_tier1_means = {}
 
-for experiment in ["4xCO2", "ghg", "aer", "lu", "anthro"]:
+for experiment in experiments:
     rfmip_tier1_means[experiment] = {}
     for model in rfmip_data:
-        if experiment in
+        rfmip_tier1_means[experiment][model] = {}
         for runid in rfmip_data[model]:
             if experiment in rfmip_data[model][runid]:
-                print(
-                    model, 
-                    runid, 
-                    experiment, 
+                rfmip_tier1_means[experiment][model][runid] = (
                     (
                         np.mean(rfmip_data[model][runid][experiment][-15:])
                     ) - (
@@ -32,6 +32,40 @@ for experiment in ["4xCO2", "ghg", "aer", "lu", "anthro"]:
                     )
                 )
 
+# Separate out the p1 and p3 from GISS and rename others
+rfmip_tier1_means["aer"]["GISS-E2-1-G_p1"] = {}
+rfmip_tier1_means["aer"]["GISS-E2-1-G_p1"]["r1i1p1f1"] = copy.deepcopy(rfmip_tier1_means["aer"]["GISS-E2-1-G"]["r1i1p1f1"])
+rfmip_tier1_means["aer"]["GISS-E2-1-G_p3"] = {}
+rfmip_tier1_means["aer"]["GISS-E2-1-G_p3"]["r1i1p3f1"] = copy.deepcopy(rfmip_tier1_means["aer"]["GISS-E2-1-G"]["r1i1p3f1"])
 
+rfmip_tier1_means["anthro"]["GISS-E2-1-G_p1"] = copy.deepcopy(rfmip_tier1_means["anthro"]["GISS-E2-1-G"])
+rfmip_tier1_means["ghg"]["GISS-E2-1-G_p1"] = copy.deepcopy(rfmip_tier1_means["ghg"]["GISS-E2-1-G"])
+rfmip_tier1_means["lu"]["GISS-E2-1-G_p1"] = copy.deepcopy(rfmip_tier1_means["lu"]["GISS-E2-1-G"])
+rfmip_tier1_means["4xCO2"]["GISS-E2-1-G_p1"] = copy.deepcopy(rfmip_tier1_means["4xCO2"]["GISS-E2-1-G"])
+
+del rfmip_tier1_means["anthro"]["GISS-E2-1-G"]
+del rfmip_tier1_means["ghg"]["GISS-E2-1-G"]
+del rfmip_tier1_means["lu"]["GISS-E2-1-G"]
+del rfmip_tier1_means["4xCO2"]["GISS-E2-1-G"]
+del rfmip_tier1_means["aer"]["GISS-E2-1-G"]
+
+
+print(rfmip_tier1_means)
+
+
+
+expt_means = {}
+for experiment in experiments:
+    expt_means[experiment] = {}
+    for model in rfmip_tier1_means[experiment]:
+        print(experiment, model)
+        n_runs = len(rfmip_tier1_means[experiment][model])
+        if n_runs > 0:
+            expt_means[experiment][model] = np.zeros((n_runs))
+            for irun, runid in enumerate(rfmip_tier1_means[experiment][model]):
+                expt_means[experiment][model][irun] = rfmip_tier1_means[experiment][model][runid]
+            expt_means[experiment][model] = np.mean(expt_means[experiment][model])
+
+print(expt_means)
 
 os.makedirs('../plots', exist_ok=True)
